d5f536615a31cffc7f316051da6c48d3030d71aa
Tim Gourley <tgourley@gmail.com>
2016-10-27 10:15:02 -0500

    Allowing spacemacs to edit git commit messages

--- init.el
+++ init.el
@@ -350,6 +350,9 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+
+  ;; Allow spacemacs to be the $EDITOR for git commit messages
+  (global-git-commit-mode t)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
